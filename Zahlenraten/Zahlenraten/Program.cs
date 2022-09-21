05bc78d83946c57209c7b5d1c442d89478e5141a
Xcics <88194161+Xcics@users.noreply.github.com>
2022-09-21 13:53:57 +0200

    Add files via upload

---
+++ Zahlenraten/Zahlenraten/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+namespace Zahlenraten
+{
+    class Program
+    {
+        static void Main()
+        {
+            int geheimzahl = new Random().Next(1, 101);
+            
+            bool richtig = false;
+            Console.WriteLine("Ihr Tipp");
+            int tipp = int.Parse(Console.ReadLine());
+
+
+            while (richtig == false)
+            {
+                
+                if (tipp > geheimzahl) {
+                    Console.WriteLine("Zu hoch");  Console.WriteLine("Neuer Tipp"); tipp = int.Parse(Console.ReadLine());
+                }
+                else if (tipp < geheimzahl) { Console.WriteLine("Zu niedrig");  Console.WriteLine("Neuer Tipp"); tipp = int.Parse(Console.ReadLine()); }
+                else if (tipp == geheimzahl) { richtig = true; Console.WriteLine("Richtig"); Console.ReadLine(); }
+                
+            }
+        }
+    }
+}
